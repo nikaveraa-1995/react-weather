930490b0d8e4aa70b6d1dfdf59a59b2f1d32effb
nikaveraa-1995 <nika.kataleya@ukr.net>
2025-03-31 14:41:11 +0300

    key servise

--- src/pages/Home/components/Home.tsx
+++ src/pages/Home/components/Home.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThisDay } from './ThisDay/ThisDay';
 import { ThisDayInfo } from './ThisDayInfo/ThisDayInfo';
 import { Days } from '../Days/Days';
 import s from './Home.module.scss';
+import { useCustomDispatch } from '../../../hooks/store';
+import { fetchCurrentWeather } from '../../../store/thunks/fetchCurrentWeather';
 
 interface Props {}
 
 export const Home = (props: Props) => {
+  const dispatch = useCustomDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCurrentWeather('paris'));
+  }, []);
+
   return (
     <div className={s.home}>
       <div className={s.wrapper}>
--- src/services/WeatherServise.ts
+++ src/services/WeatherServise.ts
@@ -1,10 +1,9 @@
-import axios, { AxiosResponse } from 'axios';
+import { AxiosResponse } from 'axios';
 import { Weather } from '../store/types/types';
+import api from '../assets/axios';
 
 export class WeatherServise {
   static getCurrentWeather(city: string): Promise<AxiosResponse<Weather>> {
-    return axios.get(
-      `api.openweathermap.org/data/2.5/weather?q=${city}&appid=e311f085eed65a20be9cef3ea488369f`,
-    );
+    return api.get<Weather>(`/weather?q=${city}`);
   }
 }
